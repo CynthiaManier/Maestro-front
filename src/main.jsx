bf98b21c4b73edd6c61f3e9b4ce19e667ceed449
CynthiaManier <cynthia.manier.pro@gmail.com>
2025-10-16 16:18:36 +0200

    client composant in progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Client from "./components/ClientList/Client/Client.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+        <Client />
+    </StrictMode>
+);
