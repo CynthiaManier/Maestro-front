e19e8e62c358cb1b30513425402ec808fa73f372
SoleneSol <so.buono@gmail.com>
2025-10-16 15:41:16 +0200

    commencement mise en forme du lecteur

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Preview from './components/Preview/Preview.jsx'
 
 function App() {
 
   return (
-
+    <>
     <h1>Maestro</h1>
+    <Preview />
+    </>
   )
 }
 
---
+++ src/components/Preview/Preview.jsx
@@ -0,0 +1,20 @@
+import "./Preview.scss";
+
+function Preview() {
+
+
+
+    return (
+        <>
+            <article className="preview__container">
+                <figure className="preview__title__container">
+                    <figcaption className="preview__title">Titre</figcaption>
+                    <audio controls></audio>
+                </figure>
+            </article>
+        </>
+    )
+
+};
+
+export default Preview;
