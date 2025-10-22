09420e0ad9703919685eb55ecae08588a3b51408
Malvina <malvina.gilardeau@gmail.com>
2025-10-22 15:24:19 +0200

    WIP composant description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,17 +4,22 @@ import './Description.scss';
 function Description() {
     return (
         <section className="description">
-            <div className="description__image">
-                <img src="partition.jpg" alt="Partition de musique" />
-            </div>
 
-            <div className="description__text">
-                <h2>
+            
+            <div className='description__title__container'>
+                <h2 className='description__title'>
                     Présentation du compositeur
                     {/* <span><img src="crayon.svg" alt="Crayon" /></span> */}
                 </h2>
+            </div>
+            
+            <div className="description__image__container">
+                <img className='description__image' src="partition.jpg" alt="Partition de musique" />
+            </div>
 
-                <p>Lorem Ipsum is simply dummy text of the printing and
+            <div className="description__text__container">
+
+                <p className='description__text'>Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when an unknown
                     printer took a galley of type and
@@ -28,7 +33,7 @@ function Description() {
                     printer took a galley of type and
                 </p>
 
-                <p>
+                <p className='description__text'>
                     Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when an unknown
