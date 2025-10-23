cefb9aff28bb6542c1598e7000cfc2a9cec6399a
CynthiaManier <cynthia.manier.pro@gmail.com>
2025-10-23 16:18:20 +0200

    Changing class names in ClientList.jsx and ClientList.scss

--- src/components/ClientList/ClientList.jsx
+++ src/components/ClientList/ClientList.jsx
@@ -6,104 +6,107 @@ import Col from "react-bootstrap/Col";
 
 function ClientList() {
     return (
-        <Container className="client-liste">
-            <Row className="client-liste-header client-liste-header-mobile">
+        <Container className="client-list">
+            {/* Header-mobile*/}
+            <Row className="client-list-header client-list-header-mobile">
                 <Col>Liste des clients</Col>
             </Row>
-            <Row className="client-liste-header client-liste-header-desktop">
+            {/* Header-desktop */}
+            <Row className="client-list-header client-list-header-desktop">
                 <Col
                     sm={2}
-                    className="client-liste-header_item client-liste-header_name_item"
+                    className="client-list-header_item client-list-header_name_item"
                 >
                     Identité
                 </Col>
                 <Col
                     sm={3}
-                    className="client-liste-header_item client-liste-header_email_item"
+                    className="client-list-header_item client-list-header_email_item"
                 >
                     Email
                 </Col>
                 <Col
                     sm={5}
-                    className="client-liste-header_item client-liste-header_adress_item"
+                    className="client-list-header_item client-list-header_adress_item"
                 >
                     Adresse
                 </Col>
                 <Col
                     sm={2}
-                    className="client-liste-header_item client-liste-header_phone-number_item"
+                    className="client-list-header_item client-list-header_phone-number_item"
                 >
                     N° de téléphone
                 </Col>
             </Row>
-            <Row className="client-liste-result">
+            {/* Lite des clients */}
+            <Row className="client-list-result">
                 <Col
                     sm={2}
-                    className="client-liste-result_item client-liste-result_name_item"
+                    className="client-list-result_item client-list-result_name_item"
                 >
                     Pierre Dupont
                 </Col>
                 <Col
                     sm={3}
-                    className="client-liste-result_item client-liste-result_email_item"
+                    className="client-list-result_item client-list-result_email_item"
                 >
                     pierre.dupont@exemple.com
                 </Col>
                 <Col
                     sm={5}
-                    className="client-liste-result_item client-liste-result_adress_item"
+                    className="client-list-result_item client-list-result_adress_item"
                 >
                     12 Avenue du Dev Fatigué 59001 Debug-sur-Mer
                 </Col>
                 <Col
                     sm={2}
-                    className="client-liste-result_item client-liste-result_phone-number_item"
+                    className="client-list-result_item client-list-result_phone-number_item"
                 >
                     0000000000
                 </Col>
             </Row>
-            <Row className="client-liste-result">
+            <Row className="client-list-result">
                 <Col
                     sm={2}
-                    className="client-liste-result_item client-liste-result_name_item"
+                    className="client-list-result_item client-list-result_name_item"
                 >
                     Lucas Taguet
                 </Col>
                 <Col
                     sm={3}
-                    className="client-liste-result_item client-liste-result_email_item"
+                    className="client-list-result_item client-list-result_email_item"
                 >
                     lucas.taguet@exemple.com
                 </Col>
                 <Col
                     sm={5}
-                    className="client-liste-result_item client-liste-result_adress_item"
+                    className="client-list-result_item client-list-result_adress_item"
                 >
                     17 Allée du Commit 75099 Paris
                 </Col>
                 <Col
                     sm={2}
-                    className="client-liste-result_item client-liste-result_phone-number_item"
+                    className="client-list-result_item client-list-result_phone-number_item"
                 >
                     1111111111
                 </Col>
             </Row>
-            <Row className="client-liste-result">
+            <Row className="client-list-result">
                 <Col
                     sm={2}
-                    className="client-liste-result_item client-liste-result_name_item"
+                    className="client-list-result_item client-list-result_name_item"
                 >
                     Yann Middleware
                 </Col>
                 <Col
                     sm={3}
-                    className="client-liste-result_item client-liste-result_email_item"
+                    className="client-list-result_item client-list-result_email_item"
                 >
                     yann.middleware@exemple.com
                 </Col>
                 <Col
                     sm={5}
-                    className="client-liste-result_item client-liste-result_adress_item"
+                    className="client-list-result_item client-list-result_adress_item"
                 >
                     Appartement 12B, Résidence des Développeurs Éclairés, 2457
                     Boulevard de la Compatibilité Internavigateurs, Quartier des
@@ -112,7 +115,7 @@ function ClientList() {
                 </Col>
                 <Col
                     sm={2}
-                    className="client-liste-result_item client-liste-result_phone-number_item"
+                    className="client-list-result_item client-list-result_phone-number_item"
                 >
                     3333333333
                 </Col>
