0bc7f83cef4b4b0b4a3f1669ce4f10bf8054148f
Malvina <malvina.gilardeau@gmail.com>
2025-10-29 19:43:20 +0100

    docs : added comment to description.js and apiDescription.js

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -5,12 +5,14 @@ import { useState, useEffect } from "react";
 function Description() {
 
 
-
+    // stocke la liste des descriptions récupérées depuis l'API
     const [descriptionList, setDescriptionList] = useState([]);
 
+    // on récupère les données depuis l'API
     async function getDescriptionList() {
         const allDescriptions = await getAllDescription();
         // console.log('allDescriptions ', allDescriptions);
+        // on appelle l'API pour avoir toutes les descriptions
         setDescriptionList(allDescriptions);
         // console.log('descriptionList',descriptionList);
     }
@@ -18,15 +20,18 @@ function Description() {
 
     useEffect(() => {
         getDescriptionList();
-    }, [])
+    }, []) // s'exécute qu'une seule fois au chargement
 
     
     return (
         <>
         <section className="description">
+            {/* Vérifie que la liste n'est pas vide avant d'afficher */}
             {descriptionList.length != 0 && 
+            // Parcourir chaque élément de la liste pour l'afficher
             descriptionList.map((description) => (
             <div key={description.id} className="description__title__container">
+                {/* Affiche le titre de chaque description */}
                 <h2 className="description__title">
                     {description.title}
                 </h2>
