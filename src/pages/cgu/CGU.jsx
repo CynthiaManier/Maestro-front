a9a0c734ceb2b64459458589120fd0d8a9488ba4
Malvina <malvina.gilardeau@gmail.com>
2025-11-21 13:34:22 +0100

    fix after pull

--- src/pages/cgu/CGU.jsx
+++ src/pages/cgu/CGU.jsx
@@ -2,14 +2,52 @@ function CGU() {
 
 return (
 <>
-<h2>home</h2>
+<h2>CGU</h2>
+
+<p>En vigueur au 00/00/00</p>
+
+<p>Bienvenue sur notre site Maestro. Veuillez lire attentivement nos conditions générales d’utilisation avant d’utiliser notre site.</p>
+
+<h3>1.Édition du site</h3>
+
+<p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli</p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli
+
+<h3>2.Hébergement</h3>2.Hébergement
+
+<p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli</p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli
+
+<h3>3.Accès Au Site</h3>3.Accès Au Site
+
+<p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli</p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli
+
+<h3>4.Propriété Intellectuelle</h3>4.Propriété Intellectuelle
+
+<p>Quod Enchiridion Epictetus stoici scripsit. Rodrigo Abela et Technologiae apud Massachusetts instituta Opera collectio.</p>Quod Enchiridion Epictetus stoici scripsit. Rodrigo Abela et Technologiae apud Massachusetts instituta Opera collectio.
+
+<h3></h3>5.Responsabilité
+
+Si ergo illa tantum fastidium compesce contra naturalem usum habili, quem habetis vestra potestate, non aliud quam
+
+
+6.Droit Application et Juridiction
+
+Si ergo illa tantum fastidium compesce contra naturalem usum habili, quem habetis vestra potestate, non aliud quam
+
+
+7.Droit Application et Juridiction Compétente
+
+Si ergo illa tantum fastidium compesce contra naturalem usum habili, quem habetis vestra potestate, non aliud quam
+
+Contact
+
+Pour toute question relative aux présentes CGU, l’utilisateur peut nous contacter via la page  Contact
+
 </>
 
-);
+    );
 }
 
 export default CGU ;
-
 
 
 
